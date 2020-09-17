8e1e63ebbfc8ee47a1c89c2c1f17d881648b4413
colinma <colinma@tencent.com>
2020-09-17 14:58:43 +0800

    Update script to avoid network problem in Tencent platform when building package

--- build_distribution_with_hive.sh
+++ build_distribution_with_hive.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-./dev/make-distribution.sh  --name tspark --pip --tgz  -Pmesos -Pyarn -Pkubernetes -Phive -Phive-thriftserver -Phadoop-2.7.2-tdw
+./dev/make-distribution.sh  --name tspark --tgz -Pyarn -Phive -Phive-thriftserver -Phadoop-2.7.2-tdw
