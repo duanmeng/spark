9bfd583fcf1861f3d8f7e8b95ed6c003d4534da4
colinma <colinma@tencent.com>
2020-10-13 15:03:08 +0800

    [story-860069213][FOLLOW-UP] Update build script to add profile spark-tencent-plugin

--- build_distribution_with_hive.sh
+++ build_distribution_with_hive.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-./dev/make-distribution.sh  --name tspark --tgz -Pyarn -Phive -Phive-thriftserver -Phadoop-2.7.2-tdw
+./dev/make-distribution.sh  --name tspark --tgz -Pyarn -Phive -Phive-thriftserver -Phadoop-2.7.2-tdw -Pspark-tencent-plugin
